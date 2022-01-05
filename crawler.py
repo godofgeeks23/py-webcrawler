655ddbed7de87d10c37a52dbd0bbd4892ee8329d
Aviral Srivastava <aviralji4@gmail.com>
2022-01-05 22:50:05 +0530

    Update crawler.py

--- crawler.py
+++ crawler.py
@@ -1,2 +1,13 @@
-# create a web crawler that can crawl a web page and save the links to a file using scrapy
-import scrapy
+my_dict = {'name': 'Jack', 'age': 26}
+
+print("Original dictionary = " ,  my_dict)
+my_dict["age"] = 27
+
+
+print("After changing age to 27 = " ,  my_dict)
+print(my_dict)
+
+
+my_dict["address"]= "Downtown"
+print("After adding new pair to dictionary = " , my_dict)
+print(my_dict)
