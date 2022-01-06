44d4e1f0416cd69af49b89e1b8213e18a1e03248
Aviral Srivastava <aviralji4@gmail.com>
2022-01-06 21:00:37 +0530

    Update crawler.py

--- crawler.py
+++ crawler.py
@@ -1,13 +1,1 @@
-my_dict = {'name': 'Jack', 'age': 26}
-
-print("Original dictionary = " ,  my_dict)
-my_dict["age"] = 27
-
-
-print("After changing age to 27 = " ,  my_dict)
-print(my_dict)
-
-
-my_dict["address"]= "Downtown"
-print("After adding new pair to dictionary = " , my_dict)
-print(my_dict)
+# updated the code base
